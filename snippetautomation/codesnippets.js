88de083642f3862705155a5d946891c872e0fc58
Tiffany Zheng <qz3@williams.edu>
2018-07-20 10:59:23 -0500

    Deploy website
    Deploy website version based on b383c45e2d6dc2272a795dbb2a15b7e5e893efe5

--- snippetautomation/codesnippets.js
+++ snippetautomation/codesnippets.js
@@ -1,91 +1,108 @@
 hljs.initHighlightingOnLoad();
 
-//Get the array of urls
+// Get the array of urls
 var allScripts = document.getElementsByTagName('script');
-var script = allScripts[allScripts.length-1];
+var script = allScripts[allScripts.length - 1];
 var sources = eval(script.getAttribute('sources'));
 var numberOfSources = sources.length;
 
-//Getting the attributes from all the code blocks
+// Getting the attributes from all the code blocks
 var allCodeBlocks = Array.from(document.getElementsByTagName('code'));
-var urls = []; //array of data from each source
+var urls = []; // array of data from each source
 
-//Throws error on invalid url
+// Throws error on invalid url
 function notValidURL(response) {
-	if(!response.ok) {
+	if (!response.ok) {
 		throw Error(response.statusText);
 	}
 	return response;
 }
 
-//Get and operate on data from source files
-for(i = 0; i < numberOfSources; i++) 
-	{
-	urls[i] = fetch(sources[i]).then(notValidURL).then(function(response) {return response.text()});
-	}
-Promise.all(urls).then(function(values) {
-	for(i = 0; i < numberOfSources; i++)
-		{
-		var dataFromSource = values[i];
-		var matchIndex = allCodeBlocks.findIndex(function(element) {
-			return element.getAttribute('data-url-index') == i.toString();
-			});	
-		
-		//For each URL, find all the code blocks with matching data-url-index 
-		while(matchIndex != -1) {		
-			
-			//Change inner HTML of code block
-			var codeBlock = allCodeBlocks[matchIndex];
-			var typeOfSnippet = codeBlock.getAttribute('data-snippet');
-			var codeChunk = "";
-			var startIndex, endindex;
-			
-			if(typeOfSnippet == "complete")
-				{
-				codeChunk = dataFromSource;
-				}
-			else if(typeOfSnippet == "portion") 
-				{
-				startIndex = dataFromSource.indexOf(codeBlock.getAttribute('data-start'));
-				
-				//Substring from index to the rest of file
-				if(codeBlock.getAttribute('data-end') === null) 
-					{
-					codeChunk = dataFromSource.substring(startIndex);
-					}
-				else 
-					{
-					endIndex = dataFromSource.indexOf(codeBlock.getAttribute('data-end'), startIndex);
-					codeChunk = dataFromSource.substring(startIndex, endIndex);
-					}
-				}
-			else
-				//If the snippet involves multiple portions, data-snippet="multipleportions"
-				{
-				var portions = eval(codeBlock.getAttribute('data-portions'));
-				for(j = 0; j < portions.length; j++)
-					{
-					startIndex = dataFromSource.indexOf(portions[j][0]);
-					
-					//Substring with start index to rest of file
-					if(portions[j].length == 1)
-						{
-						codeChunk = codeChunk + "\n" + dataFromSource.substring(startIndex);
-						}
-					else 
-						{
-						endIndex = dataFromSource.indexOf(portions[j][1],startIndex);
-						codeChunk = codeChunk + "\n" + dataFromSource.substring(startIndex, endIndex);
-						}
-					}
-				}
-			codeBlock.innerHTML = hljs.highlight('java', codeChunk).value;
-			allCodeBlocks.splice(matchIndex, 1);  //removes the element after operating on it
-			
-			matchIndex = allCodeBlocks.findIndex(function(element) {
-				return element.getAttribute('data-url-index') == i.toString();
-				});	
-			}
-		}
-});
+// Get and operate on data from source files
+for (i = 0; i < numberOfSources; i++) {
+	urls[i] = fetch(sources[i]).then(notValidURL).then(function(response) {
+		return response.text()
+	});
+}
 
+Promise
+		.all(urls)
+		.then(
+				function(values) {
+					for (i = 0; i < numberOfSources; i++) {
+						var dataFromSource = values[i];
+						var matchIndex = allCodeBlocks.findIndex(function(
+								element) {
+							return element.getAttribute('data-url-index') == i
+									.toString();
+						});
+
+						// For each URL, find all the code blocks with matching
+						// data-url-index
+						while (matchIndex != -1) {
+
+							// Change inner HTML of code block
+							var codeBlock = allCodeBlocks[matchIndex];
+							var typeOfSnippet = codeBlock
+									.getAttribute('data-snippet');
+							var codeChunk = "";
+							var startIndex, endindex;
+
+							if (typeOfSnippet == "complete") {
+								codeChunk = dataFromSource;
+							} else if (typeOfSnippet == "portion") {
+								startIndex = dataFromSource.indexOf(codeBlock
+										.getAttribute('data-start'));
+								if(startIndex < 0) throw "Start string not found at element id: " + codeBlock.id;
+
+								// Substring from index to the rest of file
+								if (codeBlock.getAttribute('data-end') === null) {
+									codeChunk = dataFromSource
+											.substring(startIndex);
+								} else {
+									endIndex = dataFromSource.indexOf(codeBlock
+											.getAttribute('data-end'),
+											startIndex);
+									codeChunk = dataFromSource.substring(
+											startIndex, endIndex);
+								}
+							} else
+							// If the snippet involves multiple portions,
+							// data-snippet="multipleportions"
+							{
+								var portions = eval(codeBlock
+										.getAttribute('data-portions'));
+								for (j = 0; j < portions.length; j++) {
+									startIndex = dataFromSource
+											.indexOf(portions[j][0]);
+
+									// Substring with start index to rest of
+									// file
+									if (portions[j].length == 1) {
+										codeChunk = codeChunk
+												+ "\n"
+												+ dataFromSource
+														.substring(startIndex);
+									} else {
+										endIndex = dataFromSource.indexOf(
+												portions[j][1], startIndex);
+										codeChunk = codeChunk
+												+ "\n"
+												+ dataFromSource.substring(
+														startIndex, endIndex);
+									}
+								}
+							}
+							codeBlock.innerHTML = hljs.highlight('java',
+									codeChunk).value;
+							allCodeBlocks.splice(matchIndex, 1);
+
+							matchIndex = allCodeBlocks
+									.findIndex(function(element) {
+										return element
+												.getAttribute('data-url-index') == i
+												.toString();
+									});
+						}
+					}
+				});
