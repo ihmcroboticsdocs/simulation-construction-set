e0a8909ca11b87c248549108099f78c7fad9dd41
Tiffany Zheng <qz3@williams.edu>
2018-08-06 14:48:34 -0500

    Deploy website
    Deploy website version based on f351cd81d365ad26665900a4438c46dd5da05248

--- snippetautomation/codesnippets.js
+++ snippetautomation/codesnippets.js
@@ -60,7 +60,7 @@ Promise
 								} else {
 									endIndex = dataFromSource.indexOf(codeBlock
 											.getAttribute('data-end'),
-											startIndex) + codeBlock.getAttribute('data-end').length;
+											startIndex);
 									if(endIndex < 0) throw "End string not found at element id: " + codeBlock.id;
 									codeChunk = dataFromSource.substring(
 											startIndex, endIndex);
